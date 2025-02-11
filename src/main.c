233c2a4688a523886915fd6b1f95c4efc23d1fe1
Lucas Emanoel <lucaasifx@gmail.com>
2025-02-10 23:22:18 -0300

    ✨ feat: Implementação da rotina de movimentação contínua do servomotor

--- src/main.c
+++ src/main.c
@@ -10,8 +10,8 @@
 #define DELAY_MS 5000 // Tempo de espera entre mudanças de posição
 #define STEP_DELAY_MS 10 // Tempo entre pequenos incrementos
 
-volatile uint8_t STEP = 0;
-
+volatile int STEP = 1250;
+bool rise = false;
 const uint16_t DUTY_180 = 6000;
 const uint16_t DUTY_90 = 3675;
 const uint16_t DUTY_0 = 1250;
@@ -27,32 +27,50 @@ uint pwm_setup(uint8_t GPIO, uint32_t wrap, float div) {
     return sliceNum;
 }
 
+void move_servo() {
+    pwm_set_gpio_level(SERVO_PIN, STEP);
+    // se estiver caindo
+    if(!rise) {
+        STEP += 12;
+        if(STEP >= DUTY_180)
+            rise = true;
+    }
+    // se estiver subindo
+    else {
+        STEP -= 12;
+        if(STEP <= DUTY_0)
+            rise = false;
+    }
+
+}
+
 int main() {
     stdio_init_all();
     sleep_ms(2000); // Aguarda inicialização
 
     uint slice = pwm_setup(SERVO_PIN, WRAP, CLK_DIV);
+    // Log de depuração
+    int clk = clock_get_hz(clk_sys);
+    uint32_t wrap = pwm_hw->slice[slice].top + 1;
+    float freq = clk / (float)(wrap * CLK_DIV);
 
+    // para demonstrar que a frequencia se mantém constante
+    printf("Frequência atual: %.2f Hz\n", freq);
+
+    printf("SERVO: 180\n");
+    pwm_set_gpio_level(SERVO_PIN, DUTY_180);
+    sleep_ms(DELAY_MS);
+
+    printf("SERVO: 90\n");
+    pwm_set_gpio_level(SERVO_PIN, DUTY_90);
+    sleep_ms(DELAY_MS);
+
+    printf("SERVO: 0\n");
+    pwm_set_gpio_level(SERVO_PIN, DUTY_0);
+    sleep_ms(DELAY_MS);
     while (true) {
-        // Log de depuração
-        int clk = clock_get_hz(clk_sys);
-        uint32_t wrap = pwm_hw->slice[slice].top + 1;
-        float freq = clk / (float)(wrap * CLK_DIV);
-        // para demonstrar que a frequencia se mantém constante
-        printf("Frequência atual: %.2f Hz\n", freq);
-
-        printf("SERVO: 180\n");
-        pwm_set_gpio_level(SERVO_PIN, DUTY_180);
-        sleep_ms(DELAY_MS);
-
-        printf("SERVO: 90\n");
-        pwm_set_gpio_level(SERVO_PIN, DUTY_90);
-        sleep_ms(DELAY_MS);
-
-        printf("SERVO: 0\n");
-        pwm_set_gpio_level(SERVO_PIN, DUTY_0);
-        sleep_ms(DELAY_MS);
-
-        puts("");
+        move_servo();
+        // delay de 10ms entre as movimentações
+        sleep_ms(10);
     }
 }
